d671dc5bc4c469dc7cf504bf42c960e2e9c47a98
Luan Ricardo <luanrnr1227@gmail.com>
2024-06-02 01:39:10 -0300

    Feat - Connected database (Mysql) and created schemas into project

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,29 @@
 import express from "express";
-// import { graphqlHTTP } from "express-graphql";
+import { graphqlHTTP } from "express-graphql";
 import cors from "cors";
-// import { createConnection } from "typeorm";
+import { createConnection } from "typeorm";
 
 const main = async () => {
+
+  await createConnection ({
+    type:"mysql",
+    database: "backend-challenge",
+    username: "root",
+    password: "password",
+    logging: true,
+    synchronize: false,
+    entities: []
+  })
   const app = express();
   app.use(cors());
   app.use(express.json());
-  // app.use("/graphql", graphqlHTTP({
-  //   schema,
-  //   graphiql: true
-  // }))
+  app.use(
+    "/graphql",
+    // graphqlHTTP({
+    //   schema,
+    //   graphiql: true,
+    // })
+  );
 
   app.listen(3001, () => {
     console.log("Server running on port 3001.");
